6443708826e3805d75028ecdee9c3f2e26ebe7d1
helixedra <greencastle08@gmail.com>
2025-05-04 23:01:10 +0200

    GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,12 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lebrowski.blog',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), partytown({ config: { forward: ['dataLayer.push'] } })],
 
   vite: {
     plugins: [tailwindcss()],
